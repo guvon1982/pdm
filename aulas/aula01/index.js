c378e433f9c1dc7ccb2dffd7dcc73bbd69e5ef16
Seu Nome <2414070017@ACADEMICO.IESB.BR>
2025-02-20 22:08:22 -0300

    codigos da aula01

--- aulas/aula01/index.js
+++ aulas/aula01/index.js
@@ -1,33 +1,24 @@
-/*
-//VARIAVEIS
+// variavies
+let variavel_local = 10; // number
+const constante = []; // array
+const objeto = {}; // objeto
+const msg = "Texto"; // string
+let status = false; // bool
 
-//variavel local = let
-let variavel_local = 10;
+console.log(1 == '1');
+console.log(1 === '1');
 
-//variavel global (NÃO USAR VAR)
-
-const constante = [];
-const objeto = {};
-const msg = "Texto"; //string
-let status = false;  //boleana
-
-//FUNCOES
-function soma(a, b){
-    return a + b;
+// funcoes
+function soma(a, b) {
+  return a + b;
 }
-const resultado = soma(1, 2);
+let resultado = soma(1, 2);
 
 const multiplica = (a, b) => a * b;
-resultado = multiplica (1, 2);
+resultado = multiplica(1, 2);
 /*
-function multiplica (a, b){
-    return a * b
-}
-*/
-
-/*
-async function tempo(duracao) {
-    return new Promise();
+function multiplica(a, b) {
+  return a * b;
 }
 */
 
@@ -40,17 +31,29 @@ async function tempo(duracao) {
 
 tempo(3000);
 
-//OBJETOS
+// objetos
 
-const pessoa = { nome: "Jose", email: "jose@iesb.edu.br", fone: "8888-8888" };
+const pessoa = { nome: "Jose", email: "jose@iesb.br", fone: "8888-8888" };
 console.log(pessoa.nome);
 pessoa.nome = "Jose Reginaldo";
 console.log(pessoa.nome);
 const { nome } = pessoa;
 console.log(nome);
-const pessoa_fisica = { cpf: 111, ...pessoa };
+const pessoa_fisica = {cpf: 111, ...pessoa};
 
 const lista = [];
 lista.push("uva");
 const achou = lista.find((item) => item === "uva");
-swd;
+/* find() equivale a 
+for(int i=0; i<lista.length; i++) {
+    if(lista[i] === "uva") {
+
+    }
+}*/
+
+const texto = "Ola";
+texto.concat(" Mundo");
+texto.toUpperCase();
+
+// modulos
+export { soma, multiplica}
