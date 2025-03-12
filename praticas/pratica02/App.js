3ba9e9b8b7c9cdee8e2f17305ab3481a4b94efee
GUvon <guvon1982@gmail.com>
2025-03-11 21:50:03 -0300

    Subindo arquivos finais da aula 11/03/2025 às 21:50

--- praticas/pratica02/App.js
+++ praticas/pratica02/App.js
@@ -1,10 +1,11 @@
 import Flexbox from "./screens/Flexbox";
 import PadraoIOS from "./screens/PadraoIOS";
 import PadraoAndroid from "./screens/PadraoIOS";
+import GaleriaScreen from "./screens/PadraoIOS";
 
 function App() {
   return (
-    <PadraoAndroid/>
+    <PadraoIOS/>
   );
 }
 
