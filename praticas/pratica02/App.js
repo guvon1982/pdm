cfb2ce1a9aa7a4f40a0edf1f2375a46b869596ca
GUvon <guvon1982@gmail.com>
2025-03-11 21:25:52 -0300

    Atualizando arquivos aula 11/03 às 21:25

--- praticas/pratica02/App.js
+++ praticas/pratica02/App.js
@@ -1,9 +1,10 @@
 import Flexbox from "./screens/Flexbox";
 import PadraoIOS from "./screens/PadraoIOS";
+import PadraoAndroid from "./screens/PadraoIOS";
 
 function App() {
   return (
-    <PadraoIOS/>
+    <PadraoAndroid/>
   );
 }
 
--- praticas/pratica02/screens/PadraoIOS.js
+++ praticas/pratica02/screens/PadraoIOS.js
@@ -1,15 +1,28 @@
-import {View } from "react-native";
+import { View, Text } from "react-native";
 
 function PadraoIOS() {
-    return (
-        <View style={{flex: 1, justifyContent: "space-between"}}>
-
-            <View style={{height: 56, backgroundColor: "gray"}}></View>
-            <View style={{height: 64, backgroundColor: "lightgray"}}></View>
-
-            
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, justifyContent: "space-between" }}>
+      <View style={{ height: 64, backgroundColor: "gray",
+        justifyContent: "center", alignItems: "center"
+       }}>
+        <Text style={{ 
+            fontSize: 24, 
+            fontWeight: "bold", 
+            color: "white" }}>
+          Meu App
+        </Text>
+      </View>
+      <View style={{ height: 80, backgroundColor: "lightgray",
+        flexDirection: "row", justifyContent: "space-around",
+        alignItems: "center"
+       }}>
+        <Text>Home</Text>
+        <Text>Perfil</Text>
+        <Text>Configurações</Text>
+      </View>
+    </View>
+  );
 }
 
 export default PadraoIOS;
