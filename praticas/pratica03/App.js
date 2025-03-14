9107cd22c80f87d96ec02025a28b0790a52ea666
guvon1982 <guvon1982@gmail.com>
2025-03-13 21:59:49 -0300

    Adicionando arquivos da pratica03

---
+++ praticas/pratica03/App.js
@@ -0,0 +1,12 @@
+import GalleryScreen from "./screens/GaleryScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ListScreen from "./screens/ListScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
+function App() {
+  return (
+    <ProfileScreen/>
+  );
+}
+
+export default App;
